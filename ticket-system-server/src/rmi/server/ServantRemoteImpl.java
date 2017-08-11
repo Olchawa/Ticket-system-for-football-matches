4eca9b62468f55f925f752a3f76120cb7a0f8680
Paweł Olchawa <pawl.olchawa@gmail.com>
2017-08-11 17:51:46 +0200

    Add serial ID

--- ticket-system-server/src/rmi/server/ServantRemoteImpl.java
+++ ticket-system-server/src/rmi/server/ServantRemoteImpl.java
@@ -7,8 +7,9 @@ import rmi.common.*;
 
 
 public class ServantRemoteImpl  extends UnicastRemoteObject implements Common {
-	
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public ServantRemoteImpl() throws RemoteException {
 	}
 	
